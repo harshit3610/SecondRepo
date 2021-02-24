626b544e40518d59adf137faa69c0b5bdf42ec4d
Arbaaz Shaikh <001arbaazshaikh@gmail.com>
2021-02-24 15:35:26 +0530

    changes add

---
+++ ArbaazJVM.java
@@ -0,0 +1,13 @@
+public class ArbaazJVM {
+
+    class Factorial_Namrata{  
+        public static void main(String args[]){  
+         int fact=1, num = 5;     
+         for(int i=1;i<=num;i++){    
+             fact=fact*i;    
+         }    
+         System.out.println("Factorial of 5  is: "+fact);    
+        }  
+       }  
+    
+}
