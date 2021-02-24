5de491534174fd70de876fd8d0b789b60624224d
Rutuja Kumbhar <rutujakumbhar98@gmail.com>
2021-02-24 15:30:30 +0530

    Added function

---
+++ RutujaFact.java
@@ -0,0 +1,21 @@
+class Demo {
+   static int factorial(int n) 
+    { 
+        if (n == 0) 
+          return 1; 
+          
+        return n*factorial(n-1); 
+    } 
+}
+class RutujaFact{
+    
+    
+    public static void main(String args[]){
+    
+        System.out.println("Hello, World!");
+        int num = 5; 
+        System.out.println("Factorial of "+ num + " is " + factorial(num)); 
+    }
+
+
+}
