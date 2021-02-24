463d15c5449f0d780d72613494b05cdefe5d84b6
vrushabh1999 <vrushabh.jain@qualitykiosk.com>
2021-02-24 10:24:26 +0530

    Factorial functionvrushabh.java

---
+++ vrushabh.java
@@ -0,0 +1,12 @@
+class factorial
+{
+    public int factorial(int value)
+    {
+    
+            int res = 1, i; 
+            for (i = 2; i <= value; i++) 
+                res *= i; 
+            return res; 
+        } 
+
+    }     
